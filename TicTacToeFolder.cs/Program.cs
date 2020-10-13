263b903b6caace0dc5b4b8eab21cd1f0f1137a2b
SumitomShome <sumitomshome@gmail.com>
2020-10-13 09:47:59 +0530

    [Sumitom] Add: Commiting UC1-CreateBoard

---
+++ TicTacToeFolder.cs/Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace TicTacToeWorkshop
+{
+    public class TicTacToeGame
+    {
+        static void Main(string[] args)
+        {
+            char[] board = CreateTheBoard();
+        }
+        //UC1//
+        public static char[] CreateTheBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 1; i < board.Length; i++)
+            {
+                board[i] = ' ';
+            }
+            return board;
+        }
+    }
+}
