11eb00a16465553ac0bb1b8e58e3255d5fbfb41e
SumitomShome <sumitomshome@gmail.com>
2020-10-13 11:25:51 +0530

    [Sumitom] Refactor: Method to take letter from user

--- TicTacToeFolder.cs/Program.cs
+++ TicTacToeFolder.cs/Program.cs
@@ -21,12 +21,28 @@ namespace TicTacToeWorkshop
         {
             Console.WriteLine("Choose between X and O to play the Tic Tac Toe Game");
             string userLetter = Console.ReadLine();
-	    while(userLetter != "X" || userLetter!= "O" || userLetter != "x" || userLetter != "o")
+	    if (userLetter == "X")
+            {
+                return char.ToUpper(userLetter[0]);
+            }
+            else if (userLetter == "O")
+            {
+                return char.ToUpper(userLetter[0]);
+            }
+            else if (userLetter == "x")
+            {
+                return char.ToUpper(userLetter[0]);
+            }
+            else if (userLetter == "o")
+            {
+                return char.ToUpper(userLetter[0]);
+            }
+            else
             {
                 Console.WriteLine("Enter valid input");
                 userLetter = Console.ReadLine();
-             }
-            return char.ToUpper(userLetter[0]);
+                return char.ToUpper(userLetter[0]);
+            }
         }
     }
 }
